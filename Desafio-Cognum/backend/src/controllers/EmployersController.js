3324af391adf2eb235ffb61b41b76268522f30fc
CaioSC01 <caio180g@gmail.com>
2023-09-12 20:21:42 -0300

    Criando e estruturando controlador do employee

---
+++ Desafio-Cognum/backend/src/controllers/EmployersController.js
@@ -0,0 +1,70 @@
+import employers from "../models/Employee.js";
+
+class EmployeeController {
+    static async listEmployers(req, res) {
+        try {
+            const resultEmployers = await employers.find({});
+            res.status(200).json(resultEmployers);
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno no servidor" });
+        }
+    }
+    static async listEmployersId(req, res) {
+        try {
+            const id = req.params.id;
+            const resultEmployers = await employers.findById(id);
+            if (resultEmployers != null) {
+                res.status(200).json(resultEmployers);
+            } else {
+                console.log("ID funcionario não encontrado");
+            }
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno no servidor" });
+        }
+    }
+    static async createEmployers(req, res) {
+        try {
+            let employee = new employers(req.body);
+            const resultEmployers = await employee.save();
+
+            res.status(200).json(resultEmployers.toJSON());
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno no servidor" });
+        }
+    }
+    static async updateEmployers(req, res) {
+        try {
+            const id = req.params.id;
+            const resultEmployers = await employers.findByIdAndUpdate(id, {
+                $set: req.body,
+            });
+
+            if (resultEmployers != null) {
+                res.status(200).send({
+                    message: "Funcionario atualizado",
+                });
+            } else {
+                console.log("ID funcionario não encontrado");
+            }
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno no servidor" });
+        }
+    }
+    static async deleteEmployers(req, res) {
+        try {
+            const id = req.params.id;
+            const resultEmployers = await employers.findByIdAndDelete(id);
+
+            if (resultEmployers != null) {
+                res.status(200).send({
+                    message: "Funcionario Removido com sucesso",
+                });
+            } else {
+                console.log("ID funcionario não encontrado");
+            }
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno no servidor" });
+        }
+    }
+}
+export default EmployeeController;

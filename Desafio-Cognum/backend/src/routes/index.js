13c817534f4058b34370b01c7e8761ad739708f7
CaioSC01 <caio180g@gmail.com>
2023-09-12 18:17:15 -0300

    Estrutura para colocar todas as rotas

--- Desafio-Cognum/backend/src/routes/index.js
+++ Desafio-Cognum/backend/src/routes/index.js
@@ -1,11 +1,12 @@
 import express from "express";
+import desafioUm from "./DesafioUm.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
         res.status(200).send({ titulo: "Desafio Cognum" });
     });
 
-    app.use(express.json());
+    app.use(express.json(), desafioUm);
 };
 
 export default routes;

0646d5fb8203cae78e579c740833210d582b522b
CaioSC01 <caio180g@gmail.com>
2023-09-12 20:23:18 -0300

    Adicionando desafio 02 no roteador principal

--- Desafio-Cognum/backend/src/routes/index.js
+++ Desafio-Cognum/backend/src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import desafioUm from "./DesafioUm.js";
+import desafioDois from "./DesafioDois.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
         res.status(200).send({ titulo: "Desafio Cognum" });
     });
 
-    app.use(express.json(), desafioUm);
+    app.use(express.json(), desafioUm, desafioDois);
 };
 
 export default routes;

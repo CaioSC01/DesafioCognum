faff39553a00adc848be0ad934d7223311682576
CaioSC01 <caio180g@gmail.com>
2023-09-12 20:22:58 -0300

    Explicando estrutura

--- Desafio-Cognum/backend/src/routes/DesafioUm.js
+++ Desafio-Cognum/backend/src/routes/DesafioUm.js
@@ -1,5 +1,7 @@
+//estruturando o desafio 01
+//Importe o express para voce poder usa-lo
 import express from "express";
-
+//uma boa pratica para colocar mais informaçoes de CRUD
 const router = express.Router();
 
 router.get("/hello", (req, res) => {

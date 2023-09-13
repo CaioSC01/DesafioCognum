13f18590a1c22d95d4fb7b932aa97283144940f3
CaioSC01 <caio180g@gmail.com>
2023-09-13 16:09:43 -0300

    Adicionando desafio 3

--- Desafio-Cognum/backend/src/routes/index.js
+++ Desafio-Cognum/backend/src/routes/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import desafioUm from "./DesafioUm.js";
 import desafioDois from "./DesafioDois.js";
+import desafioTres from "./DesafioTres.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
         res.status(200).send({ titulo: "Desafio Cognum" });
     });
 
-    app.use(express.json(), desafioUm, desafioDois);
+    app.use(express.json(), desafioUm, desafioDois, desafioTres);
 };
 
 export default routes;
